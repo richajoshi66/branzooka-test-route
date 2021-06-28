7867a5ce1cf6acd6982385c82a4a6dfefa16b63f
richajoshi66 <richajoshi66@gmail.com>
2021-06-28 12:06:04 +0530

    Updates

---
+++ precache-manifest.5e60b186aec4af4735da84638433b2b4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad941db1f5f1c9b4c879497d3a71e72",
+    "url": "/branzooka-test-route/index.html"
+  },
+  {
+    "revision": "ddcf89eba8c9600aa58a",
+    "url": "/branzooka-test-route/static/css/2.25d78810.chunk.css"
+  },
+  {
+    "revision": "36e89c16299838ed3d2c",
+    "url": "/branzooka-test-route/static/css/main.4e6475f9.chunk.css"
+  },
+  {
+    "revision": "ddcf89eba8c9600aa58a",
+    "url": "/branzooka-test-route/static/js/2.782ae476.chunk.js"
+  },
+  {
+    "revision": "e1a3eec440a54a70d5dda1771ab9d24e",
+    "url": "/branzooka-test-route/static/js/2.782ae476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e89c16299838ed3d2c",
+    "url": "/branzooka-test-route/static/js/main.b7c74f4e.chunk.js"
+  },
+  {
+    "revision": "5d0400fd1532bd2cda5a",
+    "url": "/branzooka-test-route/static/js/runtime-main.d5d56276.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/branzooka-test-route/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/branzooka-test-route/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/branzooka-test-route/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/branzooka-test-route/static/media/slick.f97e3bbf.svg"
+  }
+]);
